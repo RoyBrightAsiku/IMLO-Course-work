dd6e933eea8a55771feffb93531848bfceb3dd83
RoyBrightAsiku <roybrightasiku@gmail.com>
2024-05-08 10:58:36 +0100

    Including the neural net model

--- neuralnet.py
+++ neuralnet.py
@@ -1,4 +1,8 @@
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
 
 transform = transforms.Compose([
     transforms.Resize(256),
@@ -12,3 +16,40 @@ train_dataset = datasets.Flowers102(root='data', split='train', download=True, t
 val_dataset = datasets.Flowers102(root='data', split='val', download=True, transform=transform)
 test_dataset = datasets.Flowers102(root='data', split='test', download=True, transform=transform)
 
+# Neural Net Model
+class FlowerNet(nn.Model):
+    def __init__(self):
+        super(FlowerNet).__init__()
+        
+        self.conv1= nn.Conv2d(3,16,3,padding=1)
+        self.conv2= nn.Conv2d(16,32,3,padding=1)
+        self.conv3= nn.Conv2d(32,64,3,padding=1)
+        
+        self.pool = nn.MaxPool2d(2,2)
+        
+        self.fc1 = nn.Linear(64*28*28,512)
+        self.fc2 = nn.Linear(512, 256)
+        self.out = nn.Linear(256, 102)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        
+        x = x.view(-1, 64*28*28)
+        
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.out(x)
+
+        return x
+
+# Data loaders
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+model = FlowerNet()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
