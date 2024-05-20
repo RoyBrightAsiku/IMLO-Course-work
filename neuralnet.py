a565da8a5310ab6a0757bc1b17ca71fd334c3c2e
RoyBrightAsiku <roybrightasiku@gmail.com>
2024-05-20 11:53:27 +0100

    More robust validation loop for calculating loss

--- neuralnet.py
+++ neuralnet.py
@@ -66,25 +66,14 @@ num_epochs = 20
 #Training loop
 for epoch in range(num_epochs):
     model.train()
-    running_loss = 0.0
     for images, labels in train_loader:
-        
         images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
-        
         outputs = model(images)
-        
         loss = criterion(outputs, labels)
-        
         loss.backward()
-        
         optimizer.step()
-        
-        running_loss += loss.item() * images.size(0)
-    
-    epoch_loss = running_loss / len(train_loader.dataset)
-    print(f'Epoch {epoch+1} - Loss: {epoch_loss:.4f}')
-
+    print(f'Epoch {epoch+1} - Loss: {loss.item()}')
 
 #######                             ##########
 ########   EVALUATION AND TESTING   ##########
